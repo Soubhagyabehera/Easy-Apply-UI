826a5b52494ed0f790d7a34afe7a623f0985137c
Soubhagya Ranjan Behera <Soubhagya225bdk@gmail.com>
2025-08-08 01:28:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://easy-apply-backend-production.up.railway.app',
+        target: 'https://easy-apply-backend-production.up.railway.app',
         changeOrigin: true,
       },
     },
